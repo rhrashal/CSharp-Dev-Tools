9f58b8030cf89752da23d6f6f9a370fc7c19c0f4
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2022-04-21 14:25:04 +0600

    Update DataTable_To_Excel_Report.cs

--- DataTable_To_Excel_Report.cs
+++ DataTable_To_Excel_Report.cs
@@ -28,11 +28,14 @@
 			DataRow totalsRow = items.NewRow();
                         for (int i = 0; i < items.Columns.Count; i++)
                         {
-                            if (i>2)
+                            try
                             {
                                 string columnName = items.Columns[i].ColumnName.ToString();                               
-                                totalsRow[i] = items.AsEnumerable().Sum(row => row.Field<decimal>(columnName));                                
-                            }                            
+                                totalsRow[i] = items.AsEnumerable().Sum(row => row.Field<decimal>(columnName));        
+                            }
+                            catch (Exception ex)
+                            {
+                            }                          
                         }
                         items.Rows.Add(totalsRow);
                         //DataRow totalsRow = items.NewRow();
